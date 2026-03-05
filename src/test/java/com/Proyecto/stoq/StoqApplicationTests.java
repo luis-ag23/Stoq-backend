4e90a042f483633ee8f93acb600904ab33cd0bbd
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-03-05 18:46:12 -0400

    primer commit

---
+++ src/test/java/com/Proyecto/stoq/StoqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.stoq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
