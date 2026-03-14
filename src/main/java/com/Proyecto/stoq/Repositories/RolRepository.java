fe73539dbb15846a621845436f1b04f2131f4a6b
Harold Sejas <harold.sejas@ucb.edu.bo>
2026-03-14 10:59:28 -0400

    Fix: Documentación y orden de ramas en remoto y local

--- src/main/java/com/Proyecto/stoq/Repositories/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Proyecto.stoq.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Proyecto.stoq.Entities.Rol;
-import java.util.UUID;
-
-public interface RolRepository extends JpaRepository<Rol, UUID> {
-}
