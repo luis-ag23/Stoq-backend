0e7f4b15741c6ba4df35652e97d8ee29cea15642
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-03-15 01:11:42 -0400

    feature: entidad categoria unidades implementada

---
+++ src/main/java/com/Proyecto/stoq/domain/model/Unidad.java
@@ -0,0 +1,53 @@
+package com.Proyecto.stoq.domain.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "unidades")
+public class Unidad {
+    
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true )
+    private String nombre;
+
+    @Column(nullable = false,unique = true)
+    private String abreviatura;
+
+    @Column(nullable = false)
+    private boolean estado = true;
+
+    public Unidad() {}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }   
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
