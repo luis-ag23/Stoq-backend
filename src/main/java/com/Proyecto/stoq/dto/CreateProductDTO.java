6673905d9d8e715e73e94dcf0373fd12ca584aae
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-03-20 16:29:53 -0400

    feature: Dto para crear producto implementado

---
+++ src/main/java/com/Proyecto/stoq/dto/CreateProductDTO.java
@@ -0,0 +1,9 @@
+package com.Proyecto.stoq.dto;
+
+public class CreateProductDTO {
+    public String codigo;
+    public String nombre;
+    public String categoria;
+    public String unidad;
+    public int stock_minimo;
+}
