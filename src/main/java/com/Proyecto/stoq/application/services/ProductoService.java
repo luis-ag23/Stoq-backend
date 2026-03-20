c7e5ae03927ff3574944aa6a79ff4768166f7751
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-03-20 16:37:04 -0400

    feature: dto para actualizar producto implementado

--- src/main/java/com/Proyecto/stoq/application/services/ProductoService.java
+++ src/main/java/com/Proyecto/stoq/application/services/ProductoService.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 import com.Proyecto.stoq.domain.model.Producto;
 import com.Proyecto.stoq.dto.CreateProductDTO;
+import com.Proyecto.stoq.dto.UpdateProductDTO;
 
 
 public interface ProductoService {
@@ -10,8 +11,8 @@ public interface ProductoService {
     List<Producto> obtenerProductos();
     Optional<Producto> obtenerProductoPorId(UUID id);
     
-    void crearProducto(CreateProductDTO createProductDTO);
-    Producto actualizarProducto(UUID id, CreateProductDTO createProductDTO);
+    void crearProducto(CreateProductDTO dto);
+    Producto actualizarProducto(UUID id, UpdateProductDTO dto);
     
     void eliminarProducto(UUID id);
 }
---
+++ src/main/java/com/Proyecto/stoq/dto/UpdateProductDTO.java
@@ -0,0 +1,9 @@
+package com.Proyecto.stoq.dto;
+
+public class UpdateProductDTO {
+    public String codigo;
+    public String nombre;
+    public String categoria;
+    public String unidad;
+    public int stock_minimo;
+}
